b214fccbf1872b6656faa9f5129de121edddbcdb
Alex Mendes <mendes.develop@gmail.com>
2019-07-27 18:06:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,5 +7,13 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
   
+  board[0] | board[1]  |  board[2]
+-----------
+  board[3] | board[4]  |  board[5] 
+-----------
+  board[6] | board[7]  |  board[8]
+   
 end
+
+display_board
   
